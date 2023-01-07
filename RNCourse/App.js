27ee15755f946fc54cc7c7920a722f6f5f594740
LaupWing <loc_strict@hotmail.com>
2023-01-07 10:11:19 +0100

    Button

--- RNCourse/App.js
+++ RNCourse/App.js
@@ -1,9 +1,12 @@
-import { StyleSheet, Text, View } from "react-native"
+import { StyleSheet, Text, View, Button } from "react-native"
 
 export default function App() {
    return (
       <View style={styles.container}>
-         <Text>Open up App.js to start working on your app!</Text>
+         <Text>Test</Text>
+         <Button
+            title="TAP ME"
+         />
       </View>
    )
 }
