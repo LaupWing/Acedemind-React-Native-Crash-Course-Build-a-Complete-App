3c5abc2866a56cd695b26169d908f7c07339b9aa
LaupWing <loc_strict@hotmail.com>
2023-01-07 10:28:33 +0100

    base

--- RNCourse/App.js
+++ RNCourse/App.js
@@ -1,13 +1,23 @@
-import { StyleSheet, Text, View, Button } from "react-native"
+import { StyleSheet, Text, View, Button, TextInput } from "react-native"
 
 export default function App() {
    return (
-      <View>
-         
+      <View style={styles.appContainer}>
+         <View>
+            <TextInput
+               placeholder="Test"
+            />
+            <Button title="Add Goal"/>
+         </View>
+         <View>
+            <Text>List of goals</Text>
+         </View>
       </View>
    )
 }
 
 const styles = StyleSheet.create({
-   
+   appContainer:{
+      padding: 50
+   } 
 })
